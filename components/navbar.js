cc8dd8d2b5c51eb1d16612ca65417887b11be440
Bymoretta <axelmoretta22@gmail.com>
2022-06-21 23:07:54 -0300

    Cambio estable 1

---
+++ components/navbar.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar, Typography,Button,MenuIcon, } from "@material-ui/core";
+
+
+
+
+const Navbar = () => {
+    return (
+        <h1></h1>
+    )
+}
+
+export default Navbar
